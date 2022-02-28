cc96d9cef5f0f8852bed66206d3057411a1b5c28
Hedge <hedgefleming@gmail.com>
2022-02-28 09:02:58 -0500

    Formatted alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,7 +9,7 @@ class AlienInvasion:
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
-    
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -17,9 +17,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+
 
 if __name__ == "__main__":
     # Make a game instance and run the game.
