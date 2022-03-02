d8ceb7a0d606291850104fa7e4427abca6533d35
Hedge <hedgefleming@gmail.com>
2022-03-02 15:30:28 -0500

    Changed _update_aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -105,7 +105,8 @@ class AlienInvasion:
         self.aliens.add(alien)
     
     def _update_aliens(self):
-        """Update the positions of all aliens in the fleet."""
+        """Update the positions of all aliens in the fleet and check if aliens hit an edge."""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _check_fleet_edges(self):
