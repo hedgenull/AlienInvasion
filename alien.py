e8a586bf24916f834494ddd4d9504130208695aa
Hedge <hedgefleming@gmail.com>
2022-03-02 15:22:17 -0500

    Added check_edges, changed Alien.update

--- alien.py
+++ alien.py
@@ -22,6 +22,12 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
 
     def update(self):
-        """Move the alien to the right."""
-        self.x += self.settings.alien_speed
+        """Move the alien to the left or to the right."""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
+    
+    def check_edges(self):
+        """Return True if the alien is at an edge of the screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
