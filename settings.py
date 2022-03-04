1d37094d8412b9e5ebc119084894f6a22ada2943
Hedge <hedgefleming@gmail.com>
2022-03-04 13:05:22 -0500

    Changed settings

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 3
-        self.ship_limit = 3
+        self.ship_limit = 100
 
         # Bullet settings
         self.bullet_speed = 2
@@ -22,7 +22,7 @@ class Settings:
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 1.0
-        self.fleet_drop_speed = 4
+        self.alien_speed = 0.5
+        self.fleet_drop_speed = 2
         # A fleet direction of 1 is right, -1 is left.
         self.fleet_direction = 1
