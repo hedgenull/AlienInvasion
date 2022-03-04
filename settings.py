df895c1bf011d1024d285d6c9d2bd7a095a9e625
Hedge <hedgefleming@gmail.com>
2022-03-04 12:59:18 -0500

    Changed speed settings

--- settings.py
+++ settings.py
@@ -11,18 +11,18 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 3
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 1.5
-        self.bullet_width = 3
+        self.bullet_speed = 2
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
         # Alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 4
         # A fleet direction of 1 is right, -1 is left.
         self.fleet_direction = 1
