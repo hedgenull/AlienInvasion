d92da6cec1fd57ad9f7c4868485a9cda00e3a432
Hedge <hedgefleming@gmail.com>
2022-03-04 15:05:48 -0500

    Moved _update_screen to outside game_active

--- alien_invasion.py
+++ alien_invasion.py
@@ -41,7 +41,8 @@ class AlienInvasion:
                 self.ship.update()
                 self._update_bullets()
                 self._update_aliens()
-                self._update_screen()
+
+            self._update_screen()
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
