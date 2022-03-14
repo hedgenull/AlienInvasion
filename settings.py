61cb3ad945a6ae4134378ccc6cd419e4e9001d5f
Hedge <hedgefleming@gmail.com>
2022-03-14 11:46:55 -0400

    Changed speedup_scale

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ class Settings:
         self.fleet_direction = 1
 
         # How quickly the game speeds up
-        self.speedup_scale = 1.1
+        self.speedup_scale = 1.2
 
         self.initialize_dynamic_settings()
 
