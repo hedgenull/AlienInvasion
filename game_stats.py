6493b542f14176ea737b3fc5d22e79976902c3b8
Hedge <hedgefleming@gmail.com>
2022-03-14 16:45:36 -0400

    Updated scoring

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,9 @@ class GameStats:
         self.game_active = False
         self.reset_stats()
 
+        # High score should never be reset.
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
